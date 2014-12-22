85130d73de38da4c458f53874c33a5c2d7419759
Mikko Kaistinen <mikko.kaistinen@reaktor.fi>
2014-12-22 13:11:34 +0200

    Project setup added

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-scala-group-emailer
-===================
+scala-utils
+==============
 
-Scala API ryhmäsähköpostipalveluun.
+Scala group emailer API for OPH services.
+
+This library is currently built and published to OPH artifactory on the Bamboo CI server.
+
+To use this library, add the following dependency to your SBT build file:
+
+    "fi.vm.sade" %% "scala-group-emailer" % "0.1.0-SNAPSHOT"
---
+++ project/build.scala
@@ -0,0 +1,43 @@
+import sbt.Keys._
+import sbt._
+
+object ScalaGroupEmailerBuild extends Build {
+  val Organization = "fi.vm.sade"
+  val Name = "scala-group-emailer"
+  val Version = "0.1.0-SNAPSHOT"
+  val JavaVersion = "1.7"
+  val ScalaVersion = "2.11.1"
+  val TomcatVersion = "7.0.22"
+  val artifactory = "https://artifactory.oph.ware.fi/artifactory"
+
+  lazy val project = Project (
+    Name,
+    file("."),
+    settings = Defaults.coreDefaultSettings
+      ++ Seq(
+      organization := Organization,
+      name := Name,
+      version := Version,
+      scalaVersion := ScalaVersion,
+      javacOptions ++= Seq("-source", JavaVersion, "-target", JavaVersion),
+      scalacOptions ++= Seq("-target:jvm-1.7", "-deprecation"),
+      resolvers += "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
+      resolvers += Classpaths.typesafeReleases,
+      resolvers += "oph-sade-artifactory-releases" at artifactory + "/oph-sade-release-local",
+      resolvers += "oph-sade-artifactory-snapshots" at artifactory + "/oph-sade-snapshot-local",
+      parallelExecution in Test := false,
+      libraryDependencies ++= Seq(
+        "fi.vm.sade" %% "scala-utils" % "0.1.0-SNAPSHOT"
+      ),
+      credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+      publishTo := {
+        if (Version.trim.endsWith("SNAPSHOT"))
+          Some("snapshots" at artifactory + "/oph-sade-snapshot-local;build.timestamp=" + new java.util.Date().getTime)
+        else
+          Some("releases" at artifactory + "/oph-sade-release-local")
+      }
+    )
+  ).settings(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*)
+  lazy val projectRef: ProjectReference = project
+
+}
