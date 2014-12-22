9dbae7bfa2758cd9fe9955402fcdda1ceea98249
Mikko Kaistinen <mikko.kaistinen@reaktor.fi>
2014-12-22 14:17:05 +0200

    Settings renamed

--- src/main/scala/fi/vm/sade/groupemailer/GroupEmailerSettings.scala
+++ src/main/scala/fi/vm/sade/groupemailer/GroupEmailerSettings.scala
@@ -2,19 +2,20 @@ package fi.vm.sade.groupemailer
 
 import com.typesafe.config._
 
-case class ApplicationSettings(config: Config) extends fi.vm.sade.utils.config.ApplicationSettings(config) {
+case class GroupEmailerSettings(config: Config) extends fi.vm.sade.utils.config.ApplicationSettings(config) {
   val casUrl = config.getString("cas.url")
   val groupEmailCasUrl = config.getString("ryhmasahkoposti.cas.service")
   val groupEmailCasUsername = config.getString("ryhmasahkoposti.cas.username")
   val groupEmailCasPassword = config.getString("ryhmasahkoposti.cas.password")
   val groupEmailSessionUrl = config.getString("ryhmasahkoposti.service.session.url")
   val groupEmailServiceUrl = config.getString("ryhmasahkoposti.service.email.url")
+  val emailBatchSize = config.getInt("ryhmasahkoposti.service.batch.size")
 }
 
-case class ApplicationSettingsParser() extends fi.vm.sade.utils.config.ApplicationSettingsParser[ApplicationSettings] {
-  override def parse(config: Config) = ApplicationSettings(config)
+case class ApplicationSettingsParser() extends fi.vm.sade.utils.config.ApplicationSettingsParser[GroupEmailerSettings] {
+  override def parse(config: Config) = GroupEmailerSettings(config)
 }
 
 trait ApplicationSettingsComponent {
-  val settings: ApplicationSettings
+  val settings: GroupEmailerSettings
 }
