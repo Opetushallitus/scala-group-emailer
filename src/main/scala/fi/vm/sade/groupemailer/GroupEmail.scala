de9509b1885b79ed7dd754fed24d466428d197f0
Eetu Blomqvist <eetu.blomqvist@reaktor.fi>
2015-01-23 16:18:17 +0200

    Add languageCode to EmailInfo

--- src/main/scala/fi/vm/sade/groupemailer/GroupEmail.scala
+++ src/main/scala/fi/vm/sade/groupemailer/GroupEmail.scala
@@ -4,7 +4,7 @@ trait Content
 
 case class GroupEmail(recipient: List[Recipient], email: EmailInfo) extends Content
 case class Recipient(oid: Option[String], email: String, languageCode: String, recipientReplacements: List[Replacement], oidType: Option[String] = Some("opiskelija"))
-case class EmailInfo(callingProcess: String, templateName: String)
+case class EmailInfo(callingProcess: String, templateName: String, languageCode: String)
 case class Replacement(name: String, value: String)
 
 case class EmailData(email: EmailMessage, recipient: List[EmailRecipient]) extends Content
